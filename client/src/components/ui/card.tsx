13dac1000e6f3727822ddd3b0d3d90c473ff8870
Arapdavis01 <dancun6742@gmail.com>
2026-08-04 00:31:34 +0300

    Delete client directory

--- client/src/components/ui/card.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { cn } from '../../lib/utils';
-
-export const Card = ({ children, className }: { children: React.ReactNode; className?: string }) => (
-  <div className={cn('rounded-xl bg-gray-900 border border-gray-800 p-5', className)}>
-    {children}
-  </div>
-);
