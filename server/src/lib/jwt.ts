07715d2d0d83205a223bfb95437a92da12598088
Arapdavis01 <dancun6742@gmail.com>
2026-08-03 21:00:16 +0300

    Update jwt.ts

--- server/src/lib/jwt.ts
+++ server/src/lib/jwt.ts
@@ -1,14 +1,11 @@
 import jwt from 'jsonwebtoken';
 import { env } from '../config/env';
 
-export function signAccessToken(userId: string, expiresIn: string | number = '15m'): string {
-  const options: jwt.SignOptions = {
-    expiresIn: expiresIn as string | number,
-  };
-  return jwt.sign({ userId }, env.JWT_SECRET, options);
+export function signAccessToken(userId: string, expiresIn: string | number = '15m') {
+  return jwt.sign({ userId }, env.JWT_SECRET, { expiresIn: expiresIn as any });
 }
 
-export function signRefreshToken(userId: string): string {
+export function signRefreshToken(userId: string) {
   return jwt.sign({ userId }, env.JWT_REFRESH_SECRET, { expiresIn: '7d' });
 }
 
