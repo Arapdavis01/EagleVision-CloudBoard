39b206cd9574fac053d68ec51d9e100290d2c2b9
Arapdavis01 <dancun6742@gmail.com>
2026-08-04 21:22:29 +0300

    Update Layout.jsx

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -7,11 +7,11 @@ import CommandPalette from './CommandPalette';
 
 export default function Layout() {
   return (
-    <div className="flex h-screen bg-white dark:bg-gray-950 text-gray-900 dark:text-white">
+    <div className="flex h-screen bg-gray-950 text-white">
       <Sidebar />
-      <div className="flex-1 flex flex-col overflow-hidden">
+      <div className="flex-1 flex flex-col overflow-hidden relative">
         <RedBanner />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6 pb-16 md:pb-6">
+        <main className="flex-1 overflow-y-auto p-4 md:p-6 pb-20 md:pb-6">
           <Outlet />
         </main>
         <MobileNav />
