0189f5acf43c9ddb0f925312dc17c13ed9fc8709
Arapdavis01 <dancun6742@gmail.com>
2026-08-05 11:43:36 +0300

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  // The sslmode is already set in the URL, so we can leave this empty
 });
 module.exports = pool;
